5cc38db62e77a952d9d1e8a8736d7e54f23db75e
zajko04 <zajkoc6@gmail.com>
2019-07-07 21:50:07 +0200

    elo

--- app/src/main/java/com/example/markmycar/MainActivity.java
+++ app/src/main/java/com/example/markmycar/MainActivity.java
@@ -9,13 +9,10 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.provider.Settings;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -25,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int REQUEST_LOCATION = 1;
     Button button;
     Button googlemaps;
-    TextView textView;
+    Button found;
     LocationManager locationManager;
     String lattitude,longitude;
     SharedPreferences sharedPreferences;
@@ -39,9 +36,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 
-        textView = (TextView)findViewById(R.id.text_location);
         button = (Button)findViewById(R.id.button_location);
         googlemaps = (Button)findViewById(R.id.button);
+        found = (Button)findViewById(R.id.button2);
+
+
+
+        if(sharedPreferences.getAll() != null){
+            button.setVisibility(View.INVISIBLE);
+            googlemaps.setVisibility(View.VISIBLE);
+            found.setVisibility(View.VISIBLE);
+        }
+        else{
+            button.setVisibility(View.VISIBLE);
+            googlemaps.setVisibility(View.INVISIBLE);
+            found.setVisibility(View.INVISIBLE);
+        }
 
         button.setOnClickListener(this);
 
@@ -51,22 +61,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String s_lattitude,s_longitude;
                 s_lattitude = sharedPreferences.getString("lattitude","");
                 s_longitude= sharedPreferences.getString("longitude","");
-
-/*
-                Uri.Builder builder = new Uri.Builder();
-                builder.scheme("https")
-                        .authority("www.google.com")
-                        .appendPath("maps")
-                        .appendPath("dir")
-                        .appendPath("")
-                        .appendQueryParameter("api", "1")
-                        .appendQueryParameter("destination", s_lattitude + "," + s_longitude);
-                String url = builder.build().toString();
-                Log.d("Directions", url);
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);*/
-
                 Intent intent = new Intent(MainActivity.this,MapsActivity.class);
                 intent.putExtra("lattitude",s_lattitude);
                 intent.putExtra("longitude",s_longitude);
@@ -74,25 +68,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        updateTxt();
-
+        found.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sharedPreferences.edit().clear();
+                Toast.makeText(MainActivity.this,"Yay!",Toast.LENGTH_SHORT).show();
+                button.setVisibility(View.VISIBLE);
+                googlemaps.setVisibility(View.INVISIBLE);
+                found.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
-    public void updateTxt(){
-        String s_lattitude,s_longitude;
-        s_lattitude = sharedPreferences.getString("lattitude","");
-        s_longitude= sharedPreferences.getString("longitude","");
-        if(s_lattitude != null && s_longitude!=null){
-            textView.setText("Your last location was"+ "\n" + "Lattitude = " + s_lattitude
-                    + "\n" + "Longitude = " + s_longitude);
-        }
-    }
 
     public void saveLocation(String lattitude, String longitude){
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("lattitude",lattitude);
         editor.putString("longitude",longitude);
         editor.commit();
+        Toast.makeText(this,"Your car location has been saved.",Toast.LENGTH_SHORT).show();
+        button.setVisibility(View.INVISIBLE);
+        googlemaps.setVisibility(View.VISIBLE);
+        found.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -100,7 +97,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             buildAlertMessageNoGps();
-
         } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             getLocation();
         }
@@ -125,9 +121,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 double longi = location.getLongitude();
                 lattitude = String.valueOf(latti);
                 longitude = String.valueOf(longi);
-
-                textView.setText("Your current location is"+ "\n" + "Lattitude = " + lattitude
-                        + "\n" + "Longitude = " + longitude);
                 saveLocation(lattitude,longitude);
 
             } else  if (location1 != null) {
@@ -135,9 +128,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 double longi = location1.getLongitude();
                 lattitude = String.valueOf(latti);
                 longitude = String.valueOf(longi);
-
-                textView.setText("Your current location is"+ "\n" + "Lattitude = " + lattitude
-                        + "\n" + "Longitude = " + longitude);
                 saveLocation(lattitude,longitude);
 
             } else  if (location2 != null) {
@@ -145,20 +135,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 double longi = location2.getLongitude();
                 lattitude = String.valueOf(latti);
                 longitude = String.valueOf(longi);
-
-                textView.setText("Your current location is"+ "\n" + "Lattitude = " + lattitude
-                        + "\n" + "Longitude = " + longitude);
-                saveLocation(lattitude,longitude);
             }else{
-
                 Toast.makeText(this,"Unble to Trace your location",Toast.LENGTH_SHORT).show();
-
             }
         }
     }
 
     protected void buildAlertMessageNoGps() {
-
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Please Turn ON your GPS Connection")
                 .setCancelable(false)
--- app/src/main/java/com/example/markmycar/MapsActivity.java
+++ app/src/main/java/com/example/markmycar/MapsActivity.java
@@ -32,21 +32,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         s_longitude = intent.getStringExtra("longitude");
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng car = new LatLng( Double.parseDouble(s_lattitude),Double.parseDouble(s_longitude));
         mMap.addMarker(new MarkerOptions().position(car).title("Your car"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(car,16.0f));
